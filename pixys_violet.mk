42d5212766cd7b740c6620d311a43e100968b083
tesla59 <talktonishantsingh.ns@gmail.com>
2020-04-16 14:59:04 +0000

    violet: add miui camera

--- pixys_violet.mk
+++ pixys_violet.mk
@@ -13,13 +13,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common Pixys stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_INCLUDE_STOCK_ARCORE := true
 
 $(call inherit-product, vendor/pixys/config/common_full_phone.mk)
+
+# MIUI Camera
+$(call inherit-product-if-exist, vendor/MiuiCamera/config.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := pixys_violet
