2b42714be1f0d6f29290a4eea21d949463de145b
tesla59 <talktonishantsingh.ns@gmail.com>
2020-07-09 19:35:33 +0530

    Revert "violet: dont build parts" ok This reverts commit bbea7369184df511821a48f48cbb43b096663b44.

--- device.mk
+++ device.mk
@@ -169,6 +169,10 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne \
     services-ext
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
