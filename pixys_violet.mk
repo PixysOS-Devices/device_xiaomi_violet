4393266dd5db95aadfe89c4d62811c9bc067545c
tesla59 <nishant.maintainers@pixysos.com>
2020-07-27 12:46:37 +0000

    violet: set bootanim and gapps flag

--- pixys_violet.mk
+++ pixys_violet.mk
@@ -10,6 +10,9 @@ $(call inherit-product, device/xiaomi/violet/device.mk)
 # Inherit some common PixysOS stuff.
 $(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := pixys_violet
 PRODUCT_DEVICE := violet
