d027d68303733a6af24d43614a57ec1a7bcada25
NRanjan-17 <nalinishranjan05@gmail.com>
2023-03-07 08:42:40 +0000

    Revert "violet: Add support for Google Dialer Call recording"
    This reverts commit 4b140969aec49b6d37196a62baa541c3f2cdefdf.

--- device.mk
+++ device.mk
@@ -115,10 +115,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
-# Call recording
-PRODUCT_PACKAGES += \
-    com.google.android.apps.dialer.call_recording_audio.features.xml
-
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
