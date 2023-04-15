610fe9bb485694c36aa663607b83584f10d3a667
NRanjan-17 <nalinishranjan05@gmail.com>
2023-04-15 11:28:04 +0530

    violet: Don't Build Few apps
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -3,7 +3,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube SoundAmplifierPrebuilt talkback Photos Camera2 Aperture
+LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube SoundAmplifierPrebuilt
+LOCAL_OVERRIDES_PACKAGES += MarkupGoogle talkback Photos Camera2 Aperture GoogleTTS
+LOCAL_OVERRIDES_PACKAGES += AndroidAutoStubPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true 
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
